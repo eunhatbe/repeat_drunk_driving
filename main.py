1401d123f08d78f1a5099cfb9d58aa68cde28794
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2023-05-05 01:50:43 +0900

    test

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import requests
+import pprint
+import json
+
+# decoding Key
+service_key = open("/Users/user/PycharmProjects/repeat_drunk_driving/serviceKey.txt", 'r').readline()
+
+headers = {
+    'accept': 'application/json',
+    'Authorization': service_key,
+}
+
+params = {
+    'page': '1',
+    'perPage': '10',
+    'serviceKey': service_key,
+}
+
+response = requests.get(
+    'https://api.odcloud.kr/api/15099959/v1/uddi:4fd5b14c-8468-4cb0-93b8-9b182b1553b9',
+    params=params,
+    headers=headers,
+)
+
+if __name__ == "__main__":
+    pprint.pprint(response.json())
