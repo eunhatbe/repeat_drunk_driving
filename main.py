a8a73a83c8935c6ac8a6b9759c3aec5f03cd9522
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2023-05-07 01:11:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ service_key = open(file_url, 'r').readline()
 
 # Swagger URL
 swagger_url = "https://infuser.odcloud.kr/oas/docs?namespace=15099959/v1"
+uddi_data_list = requests.get(swagger_url).json()
+
 
 headers = {
     'accept': 'application/json',
@@ -38,30 +40,34 @@ response = requests.get(
     headers=headers,
 )
 
-def parser_uddi(url: str) -> list:
-    test_res = requests.get(url)
-    data = test_res.json()
+def parser_uddi(data: any) -> list:
     operations = []
+    temp = []
 
     for path in data["paths"]:
         for method in data["paths"][path]:
             if "operationId" in data["paths"][path][method]:
                 if data["paths"][path][method]["operationId"].startswith("getuddi:"):
                     operations.append(data["paths"][path][method]["operationId"])
+
+    for operation in operations:
+        code = operation.split(':')[1]
+        temp.append(code)
+
+    operations = temp
     return operations
+
 
 if __name__ == "__main__":
     #pprint.pprint(response.json())
     # pprint.pprint(requests.get("https://api.odcloud.kr/api/15099959/v1/uddi:4fd5b14c-8468-4cb0-93b8-9b182b1553b9?page=1&perPage=20&serviceKey=RgUQcuLZUXq5vPZffYY5065M6ioM4%2FbLIJ%2FchDQJspKPMr9Ys4FBVIVnjXd3%2BhOF6mgmQFboWsio4OZeZkXHJQ%3D%3D").json())
     #pprint.pprint(requests.get(swagger_url).json())
 
-    # test_url = "https://infuser.odcloud.kr/oas/docs?namespace=15099959/v1"
-    # test_res = requests.get(test_url)
-    # data = test_res.json()
+    uddi_list = parser_uddi(uddi_data_list)
+    big_data = []
 
-    operations = parser_uddi(swagger_url)
-    print(operations)
+    for uddi in uddi_list:
+        json_data = requests.get(f'https://api.odcloud.kr/api/15099959/v1/uddi:{uddi}',params=params,headers=headers,).json()
+        big_data.append(json_data)
 
-
-
-
+    print(big_data)
